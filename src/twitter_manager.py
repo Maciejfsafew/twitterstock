e39cd930b8a5ec5585e2503228004d59a00ea89b
kluba <dmnkluba@gmail.com>
2015-05-24 22:10:10 +0200

    Added all symbol list from nasdaq 100

--- src/twitter_manager.py
+++ src/twitter_manager.py
@@ -3,8 +3,6 @@ from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
-SYMBOLS = ["YHOO", "AAPL", "GOOG", "MSFT", "AMZN", "ADBE", "BIDU", "FB", "NVDA", "EBAY", "INTC"]
-
 class listener(StreamListener):
     def on_data(self, data):
         print data
@@ -12,7 +10,15 @@ class listener(StreamListener):
     def on_error(self, status_code):
         print status_code
 
+def read_tags():
+    with open("../symbol_list") as symbols_file:
+        symbols = symbols_file.readlines()
+    symbols = [symbol.strip('\n') for symbol in symbols]
+    return symbols
+
 def start_listening():
+    SYMBOLS = read_tags()
+
     auth = OAuthHandler(properties.twitter_key, properties.twitter_secret)
     auth.set_access_token(properties.twitter_access_token, properties.twitter_access_token_secret)
 
