4e0461eda0491b05752ede472720545e55a2298b
kluba <dmnkluba@gmail.com>
2015-05-24 23:39:21 +0200

    Added simple json parsing for incoming tweets

--- src/twitter_manager.py
+++ src/twitter_manager.py
@@ -2,19 +2,28 @@ import properties
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import json
 
 class listener(StreamListener):
     def on_data(self, data):
-        print data
+        parse_tweet(data)
         return True
     def on_error(self, status_code):
         print status_code
+
+def parse_tweet(json_tweet):
+    tweet = json.loads(json_tweet)
+    if tweet.get('text') != None:
+        print tweet['text']
+        print "\n"
 
 def read_tags():
     with open("../symbol_list") as symbols_file:
         symbols = symbols_file.readlines()
     symbols = [symbol.strip('\n') for symbol in symbols]
-    return symbols
+    hashtags = map(lambda x: "#" + x, symbols)
+    dollars = map(lambda x: "$" + x, symbols)
+    return hashtags + dollars
 
 def start_listening():
     SYMBOLS = read_tags()
